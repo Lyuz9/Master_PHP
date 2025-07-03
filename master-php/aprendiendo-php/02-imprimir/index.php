aaced37092b60423edd08fad738962215bc105aa
Lyuz9 <luisgabrielhernandezresendiz@gmail.com>
2025-07-03 11:35:36 -0600

    22.- Imprimir por pantalla

---
+++ master-php/aprendiendo-php/02-imprimir/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>imprimir</title>
+</head>
+<body>
+    <h1>2 - imprimir</h1>
+    <?php 
+        echo "<h3>Lista de videojuegos</h3>";
+        echo "<ul>"
+            . "<li>GTA SA</li>"
+            . "<li>Doom eternal</li>"
+            . "<li>Good of war ragnarok</li>"
+            . "</ul>";
+
+        echo "<p> Esta es toda"." "."la lista de juegos</p>"
+    ?>
+    <p></p>
+</body>
+</html>
