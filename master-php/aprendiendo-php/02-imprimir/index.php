218188a7a1480b6c62aad90728d023ae83a95c62
Lyuz9 <luisgabrielhernandezresendiz@gmail.com>
2025-07-03 15:59:51 -0600

    22.- Imprimir por pantalla y 23.- Uso de comentarios

--- master-php/aprendiendo-php/02-imprimir/index.php
+++ master-php/aprendiendo-php/02-imprimir/index.php
@@ -7,6 +7,10 @@
 </head>
 <body>
     <h1>2 - imprimir</h1>
+
+    <?="" // otro modo de usar echo: ?>
+    <?="Bienvenido al curso más grande de PHP"?>
+
     <?php 
         echo "<h3>Lista de videojuegos</h3>";
         echo "<ul>"
@@ -15,7 +19,14 @@
             . "<li>Good of war ragnarok</li>"
             . "</ul>";
 
-        echo "<p> Esta es toda"." "."la lista de juegos</p>"
+        echo "<p> Esta es toda"." "."la lista de juegos</p>";
+
+        // para hacer comentarios usar "//" antes de comentar
+
+        // otro modo de comentario es "/* */" ejemplo:
+        /**
+         * contenido
+         */
     ?>
     <p></p>
 </body>
