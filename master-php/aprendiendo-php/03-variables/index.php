2643495020ceed8567d45a15e57a19970a1d1963
Lyuz9 <luisgabrielhernandezresendiz@gmail.com>
2025-07-07 15:45:27 -0600

    24.- Crear variables y 25.- Más variables (sobreescribir)

--- master-php/aprendiendo-php/03-variables/index.php
+++ master-php/aprendiendo-php/03-variables/index.php
@@ -4,6 +4,19 @@ $mi_primera_variable = "Hola mundo desde una variable";
 $numero = 95;
 $verdadero = true;
 
+/*
+    se puede sobreescribir el valor en una variable, ejemplo:
+$numero = 95;
+echo "<h1>" . $numero . "</h1>";    -> mostrara 95
+
+$numero = 77;
+echo "<h1>" . $numero . "</h1>";    -> mostrara 77
+
+*/
+
+
+//$numero = 77;
+
 echo "<h1> $mi_primera_variable </h1>"; 
 echo "<h1>" . $mi_primera_variable . "</h1>";
 echo "<h1>" . $numero . "</h1>";
