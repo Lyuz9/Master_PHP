e261b412e6bcb909afcd6abd8b460e2aea72b12a
Lyuz9 <luisgabrielhernandezresendiz@gmail.com>
2025-07-07 13:58:12 -0600

    24.- Crear variables

---
+++ master-php/aprendiendo-php/03-variables/index.php
@@ -0,0 +1,13 @@
+<?php 
+
+$mi_primera_variable = "Hola mundo desde una variable";
+$numero = 95;
+$verdadero = true;
+
+echo "<h1> $mi_primera_variable </h1>"; 
+echo "<h1>" . $mi_primera_variable . "</h1>";
+echo "<h1>" . $numero . "</h1>";
+echo "<h1>" . $verdadero . "</h1>";
+
+?>
+
