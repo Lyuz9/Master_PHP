6b557e039a89e1f7acf81d0d68c90b0fe90fe943
Lyuz9 <luisgabrielhernandezresendiz@gmail.com>
2025-07-21 11:48:23 -0600

    30.- Operadores aritméticos

---
+++ master-php/aprendiendo-php/06-operadores/index.php
@@ -0,0 +1,18 @@
+<?php
+
+// operadores aritmeticos
+
+
+$numero1 = 55;
+$numero2 = 33;
+
+// suma
+echo "Suma: " . ($numero1 + $numero2) . "<br>";
+
+echo "Resta: " . ($numero1 - $numero2) . "<br>";
+
+echo "Multiplicación: " . ($numero1 * $numero2) . "<br>";
+
+echo "División: " . ($numero1 / $numero2) . "<br>";
+
+echo "Modulo: " . ($numero1 % $numero2) . "<br>";
