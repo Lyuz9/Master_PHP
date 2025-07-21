c605d8504f9a0be748f10854054a5d83cff3192f
Lyuz9 <luisgabrielhernandezresendiz@gmail.com>
2025-07-21 11:37:19 -0600

    28.- Constantes / 29.- Constantes predefinidas

--- master-php/aprendiendo-php/05-constantes/index.php
+++ master-php/aprendiendo-php/05-constantes/index.php
@@ -7,4 +7,20 @@ define('web', 'gabo.com');
 echo nombre . '<br>';
 echo web . '<br>';
 
+// constantes predefinidad
+echo PHP_OS . '<br>';
+
+echo PHP_VERSION . '<br>';
+
+echo PHP_EXTENSION_DIR . '<br>';
+
+echo __FILE__ . '<br>';
+
+function hola_file(){
+    echo __FILE__ . '<br>';
+}
+
+hola_file();
+
+echo PHP_BINARY;
 ?>
