d03460aa453887734d7083f14caf4b610a790253
Lyuz9 <luisgabrielhernandezresendiz@gmail.com>
2025-07-21 11:58:36 -0600

    31.- Operadores de incremento y decremento

--- master-php/aprendiendo-php/06-operadores/index.php
+++ master-php/aprendiendo-php/06-operadores/index.php
@@ -16,3 +16,29 @@ echo "Multiplicación: " . ($numero1 * $numero2) . "<br>";
 echo "División: " . ($numero1 / $numero2) . "<br>";
 
 echo "Modulo: " . ($numero1 % $numero2) . "<br>";
+
+
+// Operadores incremento y decremento
+
+echo "<hr>";
+
+$año = 2019;
+
+// incremento
+// $año = $año + 1
+echo $año++ . "<br>";
+
+// decremento
+// $año = año - 1
+echo $año-- . "<br>";
+
+// pre incremento
+// $año = 1 + $año
+echo ++$año . "<br>";
+
+// pre decremento
+// $año = 1 - $año
+echo --$año . "<br>";
+
+echo "Año: " . $año . "<br>";
+
